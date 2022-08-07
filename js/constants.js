dc6084821e0e7d16b804f74b64e4e9652e346bd2
Basile Parent <basile.parent.pro@gmail.com>
2022-08-07 11:34:01 +0200

    Remove old plain html/js version

--- js/constants.js
+++
@@ -1,3 +0,0 @@
-const WEBSOCKET_URL = "http://localhost:9090";
-const WEBSOCKET_PATH = "";
-const SERVER_URL = "http://localhost:9090";
--- js/index.js
+++
@@ -1,9 +0,0 @@
-const goTo = (pageId) => {
-  const list = document.querySelectorAll(".page:not(.hidden)");
-  for (let i = 0; i < list.length; ++i) {
-    list[i].classList.add("hidden");
-  }
-  setTimeout(() => {
-    document.querySelector(`#${pageId}`).classList.remove("hidden");
-  }, 300)
-}
